346d6880021e85c7d0de1b9bba877270bb1a2ac8
Will Fisher <will@teknofire.net>
2012-03-06 12:06:57 -0900

    switching sort order so newest image appears in erma app

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -7,9 +7,9 @@ class FeedsController < ApplicationController
   def show
     @feed = Feed.where(:slug => params[:slug]).first
     if params[:id].nil?
-      @entries = @feed.entries.includes(:feed).order('event_at DESC').page(params[:page])
+      @entries = @feed.entries.includes(:feed).order('event_at ASC').page(params[:page])
     else
-      @entries = @feed.entries.includes(:feed).where(:title => params[:id]).order('event_at DESC').page(params[:page])      
+      @entries = @feed.entries.includes(:feed).where(:title => params[:id]).order('event_at ASC').page(params[:page])      
     end
     respond_with @feed, @entries
   end
--- app/controllers/rss_controller.rb
+++ app/controllers/rss_controller.rb
@@ -4,9 +4,9 @@ class RssController < ApplicationController
   def show
     @feed = Feed.where(:slug => params[:slug]).first
     if params[:id].nil?
-      @entries = @feed.entries.includes(:feed).order('event_at DESC').page(params[:page])
+      @entries = @feed.entries.includes(:feed).order('event_at ASC').page(params[:page])
     else
-      @entries = @feed.entries.includes(:feed).where(:title => params[:id]).order('event_at DESC').page(params[:page])      
+      @entries = @feed.entries.includes(:feed).where(:title => params[:id]).order('event_at ASC').page(params[:page])      
     end
     respond_with @feed, @entries
   end
