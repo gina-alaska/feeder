238c15091b48be87dcea2c934bc7bc7b3a76aba2
Will Fisher <will@gina.alaska.edu>
2012-03-12 16:33:21 -0800

    fixing up the styling of various feeds

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -8,11 +8,14 @@ class FeedsController < ApplicationController
     @feed = Feed.where(:slug => params[:slug]).first
     if params[:id].nil?
       @entries = @feed.entries.includes(:feed)
+      @entries = @entries.order('event_at DESC').page(params[:page]).per(12)
+    elsif params[:id] == 'current'
+      @entries = @feed.entries.order('event_at DESC').limit(1)
     else
       @entries = @feed.entries.includes(:feed).where(:slug => params[:id])
+      @entries = @entries.order('event_at DESC').page(params[:page]).per(12)
     end
 
-    @entries = @entries.order('event_at DESC').page(params[:page]).per(12)
 
     respond_with @feed, @entries
   end
