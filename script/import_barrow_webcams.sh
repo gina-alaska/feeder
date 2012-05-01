54804114e090427d53f9bbb35c2e78584c29eb03
Web Developer <webdev@feeder-vm.gina.alaska.edu>
2012-05-01 09:13:53 -0800

    using production mode to import by default

--- script/import_barrow_webcams.sh
+++ script/import_barrow_webcams.sh
@@ -1,6 +1,6 @@
 if [ -n "$1" ]
 then
-  bundle exec rails runner "Feed.import('webcam-uaf-barrow-seaice-images', '$1')"
+  bundle exec rails runner "Feed.import('webcam-uaf-barrow-seaice-images', '$1')" -e production
 else
   echo "File name is required"
 fi
--- script/import_npp.sh
+++ script/import_npp.sh
@@ -1,6 +1,6 @@
 if [ -n "$1" ]
 then
-  bundle exec rails runner "Feed.import('npp-gina-alaska-truecolor-images', '$1')"
+  bundle exec rails runner "Feed.import('npp-gina-alaska-truecolor-images', '$1')" -e production
 else
   echo "File name is required"
 fi
