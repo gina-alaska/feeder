0f72f9ec926e2ef40a972b1ca38ab4760a9d6e94
Will Fisher <will@teknofire.net>
2012-06-04 15:31:27 -0800

    support filters by date

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -7,7 +7,12 @@ class FeedsController < ApplicationController
   
   def show
     @feed = Feed.where(:slug => params[:slug]).order('slug ASC').first
-    if params[:id].nil?
+    if params[:date]
+      year, month = params[:date].split('-')
+      date = Date.civil(year.to_i, month.to_i)
+      @entries = @feed.entries.includes(:feed).where('event_at between ? and ?', date, date.end_of_month)
+      @entries = @entries.order('event_at DESC').page(params[:page]).per(12)
+    elsif params[:id].nil?
       @entries = @feed.entries.includes(:feed)
       @entries = @entries.order('event_at DESC').page(params[:page]).per(12)
     elsif params[:id] == 'current'
@@ -16,7 +21,6 @@ class FeedsController < ApplicationController
       @entries = @feed.entries.includes(:feed).where(:slug => params[:id])
       @entries = @entries.order('event_at DESC').page(params[:page]).per(12)
     end
-
 
     respond_with @feed, @entries
   end
--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,2 +1,28 @@
 module FeedsHelper
+  def all_images_active_class(slug)
+    if current_page?(slug: slug) && params[:id].nil? && params[:date].nil?
+      "active"
+    end
+  end
+  
+  def current_image_active_class(slug)
+    if current_page?(slug: slug, id: "current")
+      "active"
+    end
+  end
+  
+  def month_images_active_class(slug, date)
+    if current_page?(slug: slug, date: date)
+      "active"
+    end
+  end
+  
+  
+  def collase_current_slug_without_date(slug)
+    if current_page?(slug: slug) && params.include?(:date)
+      ""
+    else
+      "collapse"
+    end
+  end
 end
