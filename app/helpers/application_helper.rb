4e7f005df24a1d243aa73d1b0ac6a7cfdfe9b43b
Will Fisher <will@teknofire.net>
2012-09-04 12:24:21 -0800

    making movies use cdn url as well

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,20 @@
 module ApplicationHelper
-  def feed_cdn_url
+  def feed_cdn_url(path= nil)
     if Feeder::Application.config.feed_cdn_urls.nil?
-      root_url
+      url = root_url
     else
-      Feeder::Application.config.feed_cdn_urls[rand(Feeder::Application.config.feed_cdn_urls.count)]
+      url = Feeder::Application.config.feed_cdn_urls[rand(Feeder::Application.config.feed_cdn_urls.count)]
     end
+    
+    unless path.nil?
+      url = File.join(url, path)
+    end
+      
+    url
   end
   
   def cw_image_url(img)
-    File.join(feed_cdn_url, img.url) unless img.url.nil?
+    feed_cdn_url(img.url) unless img.url.nil?
   end
   
   def feed_select_options
