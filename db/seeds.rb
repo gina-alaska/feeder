cb44c8e38ce4e0fd8df529d7b77ab023e573a409
Will Fisher <will@teknofire.net>
2012-10-19 17:54:12 -0800

    adding modis feeder worker

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,3 +45,44 @@ npp = Feed.create({
   author: 'GINA',
   where: 'POINT(-147.723056 64.843611)'  
 })
+
+Feed.create({
+  slug: 'modis-gina-alaska-naturalcolor-images',
+  title: 'MODIS Naturalcolor',
+  description: 'MODIS Naturalcolor',
+  author: 'GINA',
+  where: 'POINT(-147.723056 64.843611)'  
+})
+
+Feed.create({
+  slug: 'modis-gina-alaska-721_landcover-images',
+  title: 'MODIS 721 Landcover',
+  description: 'MODIS 721 Landcover',
+  author: 'GINA',
+  where: 'POINT(-147.723056 64.843611)'  
+})
+
+Feed.create({
+  slug: 'modis-gina-alaska-261_landcover-images',
+  title: 'MODIS 261 Landcover',
+  description: 'MODIS 261 Landcover',
+  author: 'GINA',
+  where: 'POINT(-147.723056 64.843611)'  
+})
+
+Feed.create({
+  slug: 'modis-gina-alaska-367_snowcover-images',
+  title: 'MODIS 367 Snowcover',
+  description: 'MODIS 367 Snowcover',
+  author: 'GINA',
+  where: 'POINT(-147.723056 64.843611)'  
+})
+
+Feed.create({
+  slug: 'modis-gina-alaska-thermal-images',
+  title: 'MODIS Thermal',
+  description: 'MODIS Thermal',
+  author: 'GINA',
+  where: 'POINT(-147.723056 64.843611)'  
+})
+
