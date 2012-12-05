037a27b06f898638baefc94950f399772c48b21a
Will Fisher <will@teknofire.net>
2012-12-04 16:05:32 -0900

    fixing the correct sidebar and handle cache better when paging dates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,23 @@
 //= require_tree .
 //= require_self
 
+$.extend({
+  getUrlVars: function(){
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+      hash = hashes[i].split('=');
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+    return vars;
+  },
+  getUrlVar: function(name){
+    return $.getUrlVars()[name];
+  }
+});
+
 $(function() {
   $('a:not([data-remote]):not([data-behavior]):not([data-skip-pjax])').pjax('#content', { timeout: 3000 });
   $('#feed_select select').on('change', function() {
