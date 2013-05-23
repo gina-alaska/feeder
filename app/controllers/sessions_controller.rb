a9dd3ce35a20e24d4e62bcfed9c58a1e500ac5a1
Will Fisher <will@teknofire.net>
2013-05-23 13:49:42 -0500

    missing comma

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_filter :verify_authenticity_token :only => [:create]
+  skip_before_filter :verify_authenticity_token, :only => [:create]
   
   def create
     unless @auth = Authorization.find_from_hash(auth_hash)
