5efd1d808f37c5434b67e02a4edae667a607ebca
Will Fisher <will@teknofire.net>
2013-05-23 13:46:54 -0500

    trying to fix authentication for this

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_filter :verify_authenticity_token :only => [:create]
+  
   def create
     unless @auth = Authorization.find_from_hash(auth_hash)
       @auth = Authorization.create_from_hash(auth_hash, current_user)
