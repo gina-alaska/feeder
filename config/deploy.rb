439ae0d41d87e7aecf9c9c47afaf3d46ad3d949a
Will Fisher <will@teknofire.net>
2014-05-13 14:59:26 -0800

    adding feeder cache mount to deploy and cookbook

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :log_level, :debug
 # set :pty, true
 
 set :linked_files, %w{config/database.yml config/sunspot.yml config/sidekiq.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/dragonfly }
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
