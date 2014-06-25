98194e93e4f3c56e6e59515bd15d96da4edf2396
Scott Macfarlane <scott@gina.alaska.edu>
2014-06-25 14:07:55 -0800

    Use nginx from nginx

--- cookbook/recipes/nginx.rb
+++ cookbook/recipes/nginx.rb
@@ -1,21 +1,9 @@
 app_name = "puffin"
-node.default!['nginx']['default_site_enabled'] = false
-
+node.override['nginx']['default_site_enabled'] = false
+node.override['nginx']['repo_source'] = 'nginx'
 include_recipe 'nginx'
 
-ruby_block 'move_nginx_confs' do
-  block do
-    if File.exists? '/etc/nginx/conf.d'
-      FileUtils::rm_rf '/etc/nginx/conf.d'
-    end
-  end
-end
-
-proxies = if Chef::Config[:solo]
-  []
-else
-  search(:node, 'role:haproxy').collect{|n| n['ipaddress'] }
-end
+proxies = search(:node, 'role:haproxy').collect{|n| n['ipaddress'] }
 
 template "/etc/nginx/sites-available/#{app_name}_site" do
   source 'nginx_site.erb'
