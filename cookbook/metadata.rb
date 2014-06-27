a11c4a6d211714aaea55c4c93cd27ca16e9e7b52
Scott Macfarlane <scott@gina.alaska.edu>
2014-06-27 09:49:17 -0800

    Fix inconsistant naming

--- cookbook/metadata.rb
+++ cookbook/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'will@gina.alaska.edu'
 license          'All rights reserved'
 description      'Installs/Configures puffin application server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.0.2'
 
 supports "centos", ">= 6.0"
 
