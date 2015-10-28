b471d74328d68422e3c2f5c66aa776dcf3c550fb
Scott Macfarlane <scott@gina.alaska.edu>
2015-10-28 10:49:45 -0800

    Use ENV to control redis server location

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,22 +1,10 @@
-# rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
-# rails_env = ENV['RAILS_ENV'] || 'development'
-# 
-# resque_config = YAML.load_file(rails_root + '/config/resque.yml')
-# Resque.redis = resque_config[rails_env]
-# Resque.redis.namespace = "resque:gina_puffin_feeder"
+redis_host = ENV['REDIS_HOST'] || 'localhost'
+redis_port = ENV['REDIS_PORT'] || '6379'
 Sidekiq.configure_server do |config|
-  if Rails.env == 'development'
-    config.redis = { :url => 'redis://localhost:6379/12', :namespace => "feeder_development" }
-  else
-    config.redis = { :url => 'redis://feeder-vm.gina.alaska.edu:6379/12', :namespace => "feeder_production" }
-  end
+  config.redis = { :url => "redis://#{redis_host}:#{redis_host}/12", :namespace => "feeder_#{Rails.env}" }
 end
 
 # When in Unicorn, this block needs to go in unicorn's `after_fork` callback:
 Sidekiq.configure_client do |config|
-  if Rails.env == 'development'
-    config.redis = { :url => 'redis://localhost:6379/12', :namespace => "feeder_development" }
-  else
-    config.redis = { :url => 'redis://feeder-vm.gina.alaska.edu:6379/12', :namespace => "feeder_production" }
-  end
+  config.redis = { :url => "redis://#{redis_host}:#{redis_host}/12", :namespace => "feeder_#{Rails.env}" }
 end
