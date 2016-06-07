18ca9492eb17f35c30468e7fe93d0c86426aaba9
Scott Macfarlane <scott@gina.alaska.edu>
2016-06-07 14:59:54 -0800

    Integrate devise with users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,32 +1,11 @@
 class User < ActiveRecord::Base
   include UserDevise
 
-  # has_many :authorizations
-  belongs_to :member
-
   validates_presence_of :email
   validates_uniqueness_of :email
 
+  # TODO: Remove and replace with CanCanCan
   def admin?
-    self.membership.admin?
-  end
-
-  def self.create_from_hash!(hash)
-    user = create(:name => hash['info']['name'], :email => hash['info']['email'])
-
-    user
-  end
-
-  def membership
-    if self.member.nil?
-      membership = Member.where(email: self.email).first
-      if membership.nil?
-        self.member = self.create_member(name: self.name, email: self.email)
-      else
-        self.member = membership
-      end
-      self.save
-    end
-    self.member
+    site_admin?
   end
 end
