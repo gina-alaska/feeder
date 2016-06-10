e6cc648c4d9ce4234787f63bcc04492772ae7f5d
Scott Macfarlane <scott@gina.alaska.edu>
2016-06-10 10:45:50 -0800

    Ensure generation of unique token

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,8 +1,11 @@
 class ApiKey < ActiveRecord::Base
   validates :name, presence: true, allow_blank: false
-  validates :token, presence: true, allow_blank: false
-  
+  validates :token, presence: true, uniqueness: true, allow_blank: false
+
   def self.generate_token
-    SecureRandom.base64.tr('+/=', 'Qrt')
+    loop do
+      token = SecureRandom.base64.tr('+/=', 'Qrt')
+      return token unless ApiKey.where(token: token).any?
+    end
   end
 end
