786e62eda193867c59f584a19ade6658bd96efbc
Scott Macfarlane <scott@gina.alaska.edu>
2016-06-13 10:22:43 -0800

    Start moving import bits around

--- app/workers/import_worker.rb
+++ app/workers/import_worker.rb
@@ -1,8 +1,15 @@
 class ImportWorker
   include Sidekiq::Worker
-  
+
   def perform(slug, file)
     feed = Feed.import(slug, file)
     Sunspot.commit
+  end
+
+  def perform(id)
+    import = Import.find(id)
+
+    feed = Feed.find_by(slug: import.feed)
+    feed.entries.new(image_url: import.url, event_at: import.timestamp)
   end
 end
